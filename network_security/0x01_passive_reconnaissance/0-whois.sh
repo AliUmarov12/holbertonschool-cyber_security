01d71fa293c7a50f62c53465272b959a7feb9fe7
AliUmarov12 <aliumarov2481@gmail.com>
2025-12-28 08:12:25 +0000

    ALIAS

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
+#!/bin/bash
 whois "$1" | awk '/^(Registrant|Admin|Tech)/{s=$1} /Name:|Organization:|Street:|City:|State\/Province:|Postal Code:|Country:|Phone:|Fax:|Email:/{gsub(/.*: /,""); k=$0; sub(/ .*/,"",$0); f=$0; if(f=="Street") k=k" "; if(f=="Phone"||f=="Fax") print s" "f","k ORS s" "f" Ext:,"; else print s" "f","k}' > "$1.csv"
