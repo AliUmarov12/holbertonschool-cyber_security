9bb2fe0b875cc116b42969dc79103adc8aa7cdce
AliUmarov12 <aliumarov2481@gmail.com>
2025-12-28 08:17:49 +0000

    ALIAS

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-whois "$1" | awk '/^(Registrant|Admin|Tech)/{s=$1} /Name:|Organization:|Street:|City:|State\/Province:|Postal Code:|Country:|Phone:|Fax:|Email:/{gsub(/.*: /,""); k=$0; sub(/ .*/,"",$0); f=$0; if(f=="Street") k=k" "; if(f=="Phone"||f=="Fax") print s" "f","k ORS s" "f" Ext:,"; else print s" "f","k}' > "$1.csv"
+#!/usr/bin/env bash
+whois "$1" | awk '/^(Registrant|Admin|Tech)/{s=$1} / Name:/{sub(/.*: /,"");print s" Name, "$0} / Organization:/{sub(/.*: /,"");print s" Organization, "$0} / Street:/{sub(/.*: /,"");print s" Street, "$0" "} / City:/{sub(/.*: /,"");print s" City, "$0} / State\/Province:/{sub(/.*: /,"");print s" State/Province, "$0} / Postal Code:/{sub(/.*: /,"");print s" Postal Code, "$0} / Country:/{sub(/.*: /,"");print s" Country, "$0} / Phone:/{sub(/.*: /,"");print s" Phone, "$0 ORS s" Phone Ext:, "} / Fax:/{sub(/.*: /,"");print s" Fax, " ORS s" Fax Ext:, "} / Email:/{sub(/.*: /,"");print s" Email, "$0}' > "$1.csv"
