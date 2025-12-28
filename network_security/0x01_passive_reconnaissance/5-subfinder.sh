b9bf831a8879c3d9fd9e08679460ee6f51a40a4a
AliUmarov12 <aliumarov2481@gmail.com>
2025-12-28 16:01:15 +0000

    ALIAS

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-subfinder -silent -d $1 | tee >(awk '{ cmd="dig +short "$0; cmd | getline ip; close(cmd); if(ip) print $0","ip }' > "$1.txt")
+subfinder -silent -d $1 -nW -oI -o $1.txt
+
