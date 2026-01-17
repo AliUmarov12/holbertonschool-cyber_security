454ee375962d0269997e3996338d55d8d4f772fe
AliUmarov12 <aliumarov2481@gmail.com>
2026-01-17 04:14:17 -0500

    ALIAS

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# ARGUMENT CHECK
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <NEW_HOST> <TARGET_URL> <FORM_DATA>"
+  echo "Example: $0 evil.com http://web0x00.hbtn/reset_password 'email=test@test.com'"
+  exit 1
+fi
+
 NEW_HOST="$1"
 TARGET_URL="$2"
 FORM_DATA="$3"
